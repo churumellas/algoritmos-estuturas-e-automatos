f72043ef6d69ac3caffd2e9749de6d74e3d3fe88
Almir Neto <almir.neto03@hotmail.com>
2020-07-28 07:56:09 -0300

    Finalizada implementaçao lista dupla

--- src/listadupla.h
+++ src/listadupla.h
@@ -27,6 +27,8 @@ public:
 
     void remover_todos(T valor);
 
+    void deletar_node(NodeDuplo<T> *node);
+
     bool procurar_elemento(T valor);
 
     bool vazio();
@@ -83,7 +85,6 @@ bool ListaDupla<T>::inserir_meio(T valor, int posicao){
         for(int index=0;index < posicao;index++){
             leitor = leitor->proximo;
         }
-        
         novoNode->proximo = leitor->proximo;
         leitor->proximo = novoNode;
         novoNode->anterior = leitor;
@@ -92,34 +93,69 @@ bool ListaDupla<T>::inserir_meio(T valor, int posicao){
         tamanho++;
     }
 }
+template <typename T>
+void ListaDupla<T>::deletar_node(NodeDuplo<T> *node){
+    if(node->proximo == nullptr){
+        node->anterior->proximo = nullptr;
+        node->anterior = nullptr;
+        delete[] node;
+    }else if(node->anterior == nullptr){
+        node->proximo->anterior = nullptr;
+        node->proximo = nullptr;
+        delete[] node;
+    }else{
+        node->proximo->anterior = node->anterior;
+        node->anterior->proximo = node->proximo;
+        node->proximo = nullptr;
+        node->anterior = nullptr;
+        delete[] node;
+    }
+    tamanho--;
+}
 
 template <typename T>
 void ListaDupla<T>::remover_primeiro(T valor){
     NodeDuplo<T> *leitor = cabeca;
     int posicao_atual = 0;
-    while((cabeca->valor != valor) || posicao_atual < tamanho){
+    while((leitor->valor != valor) || posicao_atual < tamanho){
         leitor = leitor->proximo;
         posicao_atual++;
     }
 
     if(leitor == nullptr){
         return
-    }else if(leitor == cabeca){
-        cabeca = leitor->proximo;
-        cabeca->anterior = nullptr;
-        leitor->proximo = nullptr;
-        free(leitor);
-    }else if(leitor == cauda){
-        cauda = cauda->anterior;
-        cauda->anterior = nullptr;
-        leitor->anterior = nullptr;
-        free(leitor);
-    }else{
-        leitor->proximo->anterior = leitor->anterior;
-        leitor->anterior->proximo = leitor->proximo;
-        leitor->proximo = nullptr;
-        leitor->anterior = nullptr;
-        free(leitor);
+    }else {
+        deletar_node(leitor);
     }
-    tamanho--;
+}
+
+template <typename T>
+void ListaDupla<T>::remover_todos(T valor){
+    NodeDuplo<T> *leitor = cabeca;
+    NodeDuplo<T> proximo_leitor;
+    while(leitor != nullptr){
+        if(leitor->valor == valor){
+            proximo_leitor = leitor->proximo;
+            deletar_node(leitor.copy());
+            leitor = proximo_leitor;
+        }else{
+            leitor = leitor->proximo;
+        }
+    }
+}
+
+template <typename T>
+bool ListaDupla<T>::procurar_elemento(T valor){
+    NodeDuplo<T> *leitor = cabeca;
+    
+    while(leitor != nullptr){
+        if(leitor->valor == valor) return true;
+        else leitor = leitor->proximo;
+    }
+}
+
+template <typename T>
+bool ListaDupla<T>::vazio(){
+    if (tamanho == 0) return true;
+    else return false;
 }
